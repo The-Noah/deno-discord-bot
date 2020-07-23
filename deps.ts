318625219a1a3cb476dab8047c217732843c26fe
The Noah <thenoahbz@gmail.com>
2020-07-23 17:04:08 -0500

    Update ping command

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export {createClient, editBotsStatus, Intents, Message, StatusType, ActivityType, sendMessage, Embed_Image, Embed_Thumbnail, Embed_Author, Embed_Field, Embed_Footer} from "https://deno.land/x/discordeno/mod.ts";
+export {createClient, editBotsStatus, Intents, Message, StatusType, ActivityType, sendMessage, editMessage, Embed_Image, Embed_Thumbnail, Embed_Author, Embed_Field, Embed_Footer} from "https://deno.land/x/discordeno@v6.2.0/mod.ts";
 export {MessageEmbed} from "./lib/embed.ts";
 export {commands} from "./lib/command.ts";
--- lib/commands/ping.ts
+++ lib/commands/ping.ts
@@ -1,9 +1,10 @@
-import {commands, Message, sendMessage} from "../../deps.ts";
+import {commands, Message, sendMessage, editMessage} from "../../deps.ts";
 
 commands.push({
   name: "Ping",
   description: "Check if bot is alive",
-  execute: (message: Message, args: string[]) => {
-    sendMessage(message.channel, "Pong!");
+  execute: async (message: Message, args: string[]) => {
+    const msg = await sendMessage(message.channel, "Pong!");
+    editMessage(msg, `:ping_pong: ${msg.timestamp - message.timestamp}ms.`);
   }
 });
